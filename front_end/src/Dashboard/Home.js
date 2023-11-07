5ed21bcecc4f48c7afac8e1e398abc9a6b72dcbb
Caleb Jordan <celp@pop-os.localdomain>
2023-11-06 22:21:07 -0600

    Fixed CSS for Register and created CSS for Home.

--- front_end/src/Dashboard/Home.js
+++ front_end/src/Dashboard/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import TicketList from "./Tickets/TicketList";
 import TopNavBar from "../TopNavBar";
+import "./Home.css";
 
 function Home(props) {
   return (
     <div>
-      <TicketList />
+      <TopNavBar />
+      <div className="Home">
+        <TicketList />
+      </div>
     </div>
   );
 }
--- front_end/src/Login/Register.js
+++ front_end/src/Login/Register.js
@@ -25,59 +25,63 @@ function Register(props) {
   };
 
   return (
-    <div className="register-form-container">
-      <h2>Register</h2>
-      <form className="register-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          value={form.email}
-          onChange={handleChange}
-          type="email"
-          placeholder="youremail@email.com"
-          id="email"
-          name="email"
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          value={form.pass}
-          onChange={handleChange}
-          type="password"
-          placeholder="********"
-          id="password"
-          name="password"
-        />
-        <label htmlFor="password">First Name:</label>
-        <input
-          value={form.firstName}
-          onChange={handleChange}
-          type="firstName"
-          placeholder="First Name"
-          id="firstName"
-          name="firstName"
-        />
-        <label htmlFor="password">Last Name:</label>
-        <input
-          value={form.lastName}
-          onChange={handleChange}
-          type="lastName"
-          placeholder="Last Name"
-          id="lastName"
-          name="lastName"
-        />
-        <label htmlFor="password">Phone Number:</label>
-        <input
-          value={form.phone}
-          onChange={handleChange}
-          type="phone"
-          placeholder="12345678901"
-          id="phone"
-          name="phone"
-        />
-        <button onSubmit={handleSubmit} type="submit">
-          Register
+    <div className="Register">
+      <div className="register-form-container">
+        <h2>Register</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input
+            value={form.email}
+            onChange={handleChange}
+            type="email"
+            placeholder="youremail@email.com"
+            id="email"
+            name="email"
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            value={form.pass}
+            onChange={handleChange}
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+          />
+          <label htmlFor="password">First Name:</label>
+          <input
+            value={form.firstName}
+            onChange={handleChange}
+            type="firstName"
+            placeholder="First Name"
+            id="firstName"
+            name="firstName"
+          />
+          <label htmlFor="password">Last Name:</label>
+          <input
+            value={form.lastName}
+            onChange={handleChange}
+            type="lastName"
+            placeholder="Last Name"
+            id="lastName"
+            name="lastName"
+          />
+          <label htmlFor="password">Phone Number:</label>
+          <input
+            value={form.phone}
+            onChange={handleChange}
+            type="phone"
+            placeholder="12345678901"
+            id="phone"
+            name="phone"
+          />
+          <button onSubmit={handleSubmit} type="submit">
+            Register
+          </button>
+        </form>
+        <button className="link-btn">
+          Already have an account? Login here.
         </button>
-      </form>
-      <button className="link-btn">Already have an account? Login here.</button>
+      </div>
     </div>
   );
 }
