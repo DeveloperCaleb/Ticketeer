c64132779b7f439a1885e87a95509afe392bacd8
Caleb Jordan <celp@pop-os.localdomain>
2023-11-07 21:52:56 -0600

    Clicking on ticket routes to ticket details page

--- front_end/src/Dashboard/Tickets/Ticket.js
+++ front_end/src/Dashboard/Tickets/Ticket.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import { tickets } from "../../Test DB/tickets";
 
 function Ticket(props) {
+  let navigate = useNavigate();
   return tickets.map((ticket) => {
     return (
-      <tr key={ticket.TicketID}>
+      <tr
+        key={ticket.TicketID}
+        onClick={() => {
+          navigate(`/ticket/${ticket.TicketID}`);
+        }}
+      >
         <td>{ticket.CreatedBy}</td>
         <td>{ticket.CreatedBy}</td>
         <td>{ticket.Title}</td>
--- front_end/src/Dashboard/Tickets/TicketList.js
+++ front_end/src/Dashboard/Tickets/TicketList.js
@@ -1,23 +1,26 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 import Ticket from "./Ticket";
+import "./TicketList.css";
 
 function TicketList(props) {
   return (
-    <Table striped bordered hover variant="light">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Username</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <Ticket />
-      </tbody>
-    </Table>
+    <div className="table-container">
+      <Table striped bordered hover variant="light">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Ticket />
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
