9442ae59d5a872cd83f285bded4c9c9dfbf16260
Caleb Jordan <celp@pop-os.localdomain>
2023-11-08 22:00:28 -0600

    Ticket Detail Page
    Ticket details page now shows details of currently selected ticket.

--- front_end/src/Dashboard/Tickets/TicketDetails.js
+++ front_end/src/Dashboard/Tickets/TicketDetails.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { tickets } from "../../Test DB/tickets";
 
 function TicketDetails(props) {
-  return <div>ticket details</div>;
+  const { ticketId } = useParams();
+
+  const ticket = tickets.find((ticket) => {
+    return ticket.TicketID.toString() === ticketId;
+  });
+
+  return (
+    <div>
+      <div>Title: {ticket.Title}</div>
+      <div>Ticket ID: {ticket.TicketID}</div>
+      <div>Created At: {ticket.CreatedAt}</div>
+      <div>Updated At: {ticket.UpdatedAt}</div>
+      <div>Description: {ticket.Description}</div>
+      <div>Assigned to: {ticket.AssignedTo}</div>
+    </div>
+  );
 }
 
 export default TicketDetails;
